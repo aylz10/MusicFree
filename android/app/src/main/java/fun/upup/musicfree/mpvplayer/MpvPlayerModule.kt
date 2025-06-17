34c6633a8604ce0b28b5ab0e80e5319748043060
CTZZG <41315541+CTZZG@users.noreply.github.com>
2025-06-17 19:12:36 +0800

    Update MpvPlayerModule.kt

--- android/app/src/main/java/fun/upup/musicfree/mpvplayer/MpvPlayerModule.kt
+++ android/app/src/main/java/fun/upup/musicfree/mpvplayer/MpvPlayerModule.kt
@@ -3,6 +3,7 @@ package fun.upup.musicfree.mpvplayer
 import android.util.Log
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
+import com.facebook.react.bridge.UiThreadUtil
 import is.xyz.mpv.MPVLib
 import java.util.*
 import java.util.concurrent.Executors
